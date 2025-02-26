2ed8745daa198c86ae33c884357b26ea2d509e8e
Mary96-blip <mariacarla.ciavattone@gmail.com>
2025-02-26 02:20:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
-const fs = require('fs'); // Importa il modulo per leggere e scrivere file
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.json()); // Permette di leggere il corpo delle richieste in formato JSON
-app.use(express.static('public')); // Serve la cartella 'public' per il sito
-
-// Percorso per ottenere i messaggi
-app.get('/messages', (req, res) => {
-    fs.readFile('messages.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Errore nella lettura dei messaggi');
-        }
-        res.send(JSON.parse(data)); // Invia i messaggi in formato JSON
-    });
-});
-
-// Percorso per aggiungere un messaggio
 app.post('/messages', (req, res) => {
-    const newMessage = req.body.message;
+    const newMessage = req.body.message; // Prendi il messaggio inviato
 
+    if (!newMessage) {
+        return res.status(400).send('Messaggio vuoto'); // Se non c'è un messaggio, restituisci un errore
+    }
+
+    // Leggi i messaggi esistenti
     fs.readFile('messages.json', 'utf8', (err, data) => {
         if (err) {
             return res.status(500).send('Errore nella lettura dei messaggi');
         }
 
-        const messages = JSON.parse(data);
-        messages.push(newMessage);
+        const messages = JSON.parse(data); // Trasforma i dati in un array
+        messages.push(newMessage); // Aggiungi il nuovo messaggio
 
+        // Salva il nuovo array nel file
         fs.writeFile('messages.json', JSON.stringify(messages, null, 2), (err) => {
             if (err) {
                 return res.status(500).send('Errore nel salvataggio del messaggio');
@@ -35,9 +22,4 @@ app.post('/messages', (req, res) => {
             res.status(200).send('Messaggio aggiunto con successo');
         });
     });
-});
-
-// Avvia il server
-app.listen(port, () => {
-    console.log(`Server in ascolto su http://localhost:${port}`);
 });
